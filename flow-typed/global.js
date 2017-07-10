f50023ffade04bcc741215eabfd5bde0e0d661c2
Adam Bergman <adam@fransvilhelm.com>
2017-07-10 12:19:59 +0200

    Create component <Icons /> (#3)
    * :art: Set up Eslint
    
    * :art: Set up Storybook
    
    * :art: Set up Prettier with precommit hook
    
    * :heavy_plus_sign: Install styled-components
    
    * :heavy_plus_sign: Install dependencies
    
    * :rotating_light: Update linter rules
    
    * :art: Add icons
    
    This commit adds an <Icon /> component with different icons available.
    
    * :heavy_plus_sign: Install flow-bin
    
    * :heavy_plus_sign: Install lib-defs
    
    * :art: Set up flow
    
    * :heavy_plus_sign: Install styled-components
    
    * :heavy_plus_sign: Install dependencies
    
    * :rotating_light: Update linter rules
    
    * :art: Add icons
    
    This commit adds an <Icon /> component with different icons available.
    
    * :heavy_plus_sign: Install libdefs
    
    * :wrench: Configuring flow
    
    * :wrench: Configuring eslint rules
    
    * :rotating_light: Adding flow types

---
+++ flow-typed/global.js
@@ -0,0 +1,13 @@
+/* eslint-disable no-undef */
+/* @flow */
+
+declare type IconType =
+  | 'inbox'
+  | 'settings'
+  | 'plus-circle'
+  | 'list'
+  | 'group'
+  | 'group-circle'
+  | 'more-vertical'
+  | 'edit'
+  | 'trash';
---
+++ src/components/Icon/index.js
@@ -0,0 +1,55 @@
+// @flow
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { compose, call, propOr, __ } from 'ramda';
+import { toStudlyCaps } from 'strman';
+
+import * as icons from './Icons';
+
+const Container = styled.span`
+  position: relative;
+  display: inline-flex;
+  align-self: center;
+  height: 1em;
+  width: 1em;
+`;
+
+const Svg = styled.svg`
+  width: 1em;
+  height: 1em;
+  fill: none;
+  stroke: ${props => (props.active ? '#2F80ED' : '#333333')};
+  stroke-width: 2;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+
+  ${props =>
+    props.inline &&
+    css`
+    position: absolute;
+    bottom: -0.123em;
+  `};
+`;
+
+const getIcon: (icon: IconType) => React$Element<*> = compose(
+  call,
+  propOr(() => <g />, __, icons),
+  toStudlyCaps,
+);
+
+const Icon = ({
+  icon,
+  inline,
+  active,
+}: {
+  icon: IconType,
+  inline?: boolean,
+  active?: boolean,
+}) =>
+  <Container>
+    <Svg viewBox="0 0 24 24" inline={inline} active={active}>
+      {getIcon(icon)}
+    </Svg>
+  </Container>;
+
+export default Icon;
